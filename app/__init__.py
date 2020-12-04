8dbeaff674c4b599b4bafb828935481276e383bd
Lucas Gabriel <lucasgabriel317@gmail.com>
2020-12-04 13:48:54 -0300

    config CORS

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from pymongo import MongoClient
 from flask_bcrypt import Bcrypt
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 app.config.from_object('config')
 
 client = MongoClient('mongodb://localhost:27017/')
--- config.py
+++ config.py
@@ -1,2 +1,1 @@
-SECRETKEY = '123'
 DEBUG = True 
